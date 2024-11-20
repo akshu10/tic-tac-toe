429cad7684e9c0092ea82a8ecc8268390d0d36d8
Parth Patel <akshup66@gmail.com>
2024-11-19 21:10:42 -0500

    Update layout and page styles for improved accessibility and aesthetics

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,19 +7,18 @@ export default function Home() {
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
         onSubmit={(e) => {
           e.preventDefault();
-
           console.log("submit");
         }}
       >
         <div className="mb-4">
           <label
-            className="block text-gray-700 font-bold mb-5 text-lg tracking-wider"
+            className="block text-black font-bold mb-5 text-lg tracking-wider"
             htmlFor="game-room-id"
           >
             Game Room ID
           </label>
           <input
-            className="border rounded-md w-full h-14 py-2 px-3 mb-6 text-gray-700 ring-2 focus:ring-lime-500 hover:ring-lime-500 focus:outline-none focus:shadow-outline tracking-widest"
+            className="border rounded-md w-full h-14 py-2 px-3 mb-6 text-black placeholder-gray-400 bg-gray-200 ring-2 focus:ring-lime-500 hover:ring-lime-500 focus:outline-none focus:shadow-outline tracking-widest"
             id="game-room-id"
             type="text"
             placeholder="5wec1"
