0817b215368fe341ea64ddaf136208d0f5609097
Parth Patel <akshup66@gmail.com>
2024-11-23 16:28:58 -0500

    Clear the port occupied by the server when restarting or on SIGTERM

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import { Server, RemoteSocket, DefaultEventsMap, Socket } from "socket.io";
 
+import { exec } from "child_process";
+
 import { PORT } from "./config";
 import logger from "./logger";
 
@@ -7,7 +9,7 @@ logger.info({ PORT });
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
   },
 });
 
@@ -15,40 +17,68 @@ io.attach(PORT);
 
 // Array to track connected sockets
 const connectedSockets: Socket[] = [];
-
-io.on("disconnecting", () => {
-  console.log("IO disconnect");
-});
+const gameRooms: Set<string> = new Set();
 
 io.on("connection", async (socket) => {
+  // Emit a connection event to the client
+  socket.emit("connection", { id: socket.id });
+
   connectedSockets.push(socket);
 
-  console.log(
-    "allSockets",
-    connectedSockets.length,
-    connectedSockets.map((s) => s.id)
-  );
+  // Register event listeners for each socket here
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected", socket.id);
+  });
 
-  reset();
+  socket.on("createOrJoin", (data) => {
+    console.log("createOrJoin", data);
+
+    socket.join(data.gameId);
+    console.log(`Socket ${socket.id} joined room ${data.gameId}`);
+    gameRooms.add(socket.data.gameId);
+
+    // this will broadcast to all sockets in the room except the current
+    // this is now working.
+    socket
+      .to(data.gameId)
+      .emit("playerJoined", { secondPlayer: "Player 2", gameId: data.gameId });
+  });
 });
 
-connectedSockets.forEach((s) => {
-  s.on("connect", () => {
-    logger.info({ message: "Client Connected!", id: s.id });
-  });
+io.of("/default").adapter.on("create-room", (room) => {
+  console.log(`room ${room} was created`);
+});
 
-  s.on("disconnecting", () => {
-    logger.info(`Socket ${s.id} will be disconnected`);
-    logger.info({ message: "Client Disconnected!", id: s.id });
-  });
+io.of("/default").adapter.on("join-room", (room, id) => {
+  console.log(`socket ${id} has joined room ${room}`);
 });
 
 function reset() {
-  logger.info("resetting");
+  logger.info(`resetting  ${connectedSockets.length} sockets`);
   connectedSockets.forEach((s) => {
     console.log("disconnecting", s.id);
     s.disconnect();
   });
 }
 
-// reset();
+function killProcess(port: number) {
+  return new Promise((resolve, reject) => {
+    exec(`npx kill-port ${port}`, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) => {
+  process.on(signal, async () => {
+    console.log(`Received ${signal}, cleaning up...`);
+    reset();
+    // Perform cleanup operations here
+    await killProcess(PORT);
+    process.exit();
+  });
+});
