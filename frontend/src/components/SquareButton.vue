9265ec7a0683b70a3f4d597da38be2924a4636b5
Parth Patel <akshup66@gmail.com>
2024-11-24 17:19:16 -0500

    Update SquareButton component: allow null value and increase text size

--- frontend/src/components/SquareButton.vue
+++ frontend/src/components/SquareButton.vue
@@ -2,13 +2,13 @@
 import { defineProps } from 'vue'
 
 defineProps<{
-  value: string
+  value: string | null
   onClick: (payload: MouseEvent) => void
 }>()
 </script>
 
 <template>
-  <button @click="onClick" class="bg-white w-20 h-16 text-lg">
+  <button @click="onClick" class="bg-white w-20 h-16 text-3xl">
     {{ value }}
   </button>
 </template>
