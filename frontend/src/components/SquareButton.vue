c4a04ca5677f627c5725bd554f2dd957d0f76d80
Parth Patel <akshup66@gmail.com>
2024-11-24 15:31:31 -0500

    Add SquareButton component for reusable button functionality

---
+++ frontend/src/components/SquareButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps<{
+  value: string
+  onClick: (payload: MouseEvent) => void
+}>()
+</script>
+
+<template>
+  <button @click="onClick" class="border bg-white border-black w-20 h-16 text-lg">
+    {{ value }}
+  </button>
+</template>
