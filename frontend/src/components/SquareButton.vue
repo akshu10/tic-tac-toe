25f561645bbd8125ca942f590be9d5b01de81908
Parth Patel <akshup66@gmail.com>
2024-11-27 21:45:32 -0500

    Add disabled state to SquareButton component

--- frontend/src/components/SquareButton.vue
+++ frontend/src/components/SquareButton.vue
@@ -4,11 +4,12 @@ import { defineProps } from 'vue'
 defineProps<{
   value: string | null
   onClick: (payload: MouseEvent) => void
+  disabled: boolean
 }>()
 </script>
 
 <template>
-  <button @click="onClick" class="bg-white w-20 h-16 text-3xl">
+  <button @click="onClick" class="bg-white w-20 h-16 text-3xl" :disabled="disabled">
     {{ value }}
   </button>
 </template>
